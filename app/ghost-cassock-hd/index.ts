4156064eae386782f025ebd29197f8f41bc726e5
Mohammad <98p7rcf5mc@privaterelay.appleid.com>
2026-07-18 20:36:52 +0200

    Assemble Ghost Cassock HD asset

---
+++ app/ghost-cassock-hd/index.ts
@@ -0,0 +1,5 @@
+import part00 from "./part-00";
+import part01 from "./part-01";
+import part02 from "./part-02";
+
+export const GHOST_CASSOCK_HD = `data:image/webp;base64,${part00}${part01}${part02}`;
